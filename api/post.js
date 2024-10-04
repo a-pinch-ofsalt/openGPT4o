783ec844734182c8682ab0a447b0889acc792272
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-10-04 01:20:00 -0400

    Changed to js for vercel compatibility

---
+++ api/post.js
@@ -0,0 +1,23 @@
+
+import { Client } from "@gradio/client";
+
+const client = await Client.connect("KingNish/OpenGPT-4o");
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+      // Get data from the request body
+      const prompt = req.body;
+
+    const result = await client.predict("/chat", { 		
+		user_prompt: {"text": prompt,"files":[]}, 
+    });
+  
+      // Respond with a success message
+      res.status(200).json({ result: result.data });
+    } else {
+      // If not a POST request, return a 405 method not allowed
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  }
+  
